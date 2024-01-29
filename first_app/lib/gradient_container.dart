45b1cb845dedcdf682bcfebecbcda4cc1c4a48fe
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 14:51:11 +0100

    feat: add a styled text button named Roll Dice and center using mainAxisSize

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -8,6 +8,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  rollDice() {}
+
   @override
   Widget build(context) {
     return Container(
@@ -19,11 +21,27 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-1.png',
-          width: 200,
-        ),
-      ),
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/dice-1.png',
+            width: 200,
+          ),
+          const SizedBox(height: 20,), // Create an empty box of heigth 20
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(top: 20,),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              ),
+            ),
+            child: const Text('Roll Dice'),
+          )
+        ],
+      )),
     );
   }
 }
