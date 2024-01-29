8b9081b17137e9cd83cc22cd944eed0125b16d2a
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 11:49:46 +0100

    refactor: split code accross files and create custome widgets

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,38 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
       body: GradientContainer(),
     ),
   ));
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 90)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
