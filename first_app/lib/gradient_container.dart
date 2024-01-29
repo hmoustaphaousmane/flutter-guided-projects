b73164ad973083a95375fbeb1a77455b1956e92d
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 12:29:28 +0100

    feat: use variables (var, const, final), and preperties to make widget dynamic

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -2,25 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/styled_text.dart';
 
+const startAligment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 90)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAligment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText(),
-      ),
+          child: StyledText('Hello World!')
+          ),
     );
   }
 }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,7 +5,11 @@ import 'package:first_app/gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer(
+        colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 90),
+        ],),
     ),
   ));
 }
