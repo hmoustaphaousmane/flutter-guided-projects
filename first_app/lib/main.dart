9763a0b510a9bd707ed29b8150fb4928096e545b
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 11:12:44 +0100

    style: user gradient colors

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 63, 5, 120),
-      body: Center(
-        child: Text('Hello World!'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 90)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text('Hello World!'),
+        ),
       ),
     ),
   ));
