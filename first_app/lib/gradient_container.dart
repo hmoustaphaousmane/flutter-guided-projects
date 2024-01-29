58d4ae53ed43126012e507c469cbefcde7a5145e
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 14:43:14 +0100

    feat: display image, use multiple constrators

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:first_app/styled_text.dart';
-
 const startAligment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -20,9 +18,12 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-          child: StyledText('Hello World!')
-          ),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
+      ),
     );
   }
 }
