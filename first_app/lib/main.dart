94bcc7b981ba1c0fe59874d9a6e0d73151a59d76
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-29 10:39:22 +0100

    feat: center widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World!'),));
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    ),
+  ));
 }
