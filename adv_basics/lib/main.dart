afebf4571a157e519007ae4d6ec8402583ec46ec
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-30 16:32:28 +0100

    feat: create quiz and questions screens

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-        home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.purple, Colors.indigo],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    )),
+    const Quiz(),
   );
 }
