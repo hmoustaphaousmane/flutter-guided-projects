e43a3bf3b535b4715b1cb90835137bd20d1f0726
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-31 11:46:58 +0100

    reactor: render content conditionaly using ternary expression

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -13,19 +13,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -38,7 +33,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
