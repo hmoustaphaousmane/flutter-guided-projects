809ae326613056380969b64554324cb4dad828fd
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-31 15:16:40 +0100

    feat: manage the questions index as state (go from one question to another after answering it

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -13,9 +13,19 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+    currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -36,7 +46,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswers().map((item) { // Spred operator: ...
               return AnswerButton(
                 answerText: item,
-                onTap: () {},
+                onTap: answerQuestion,
               );
             }),
           ],
