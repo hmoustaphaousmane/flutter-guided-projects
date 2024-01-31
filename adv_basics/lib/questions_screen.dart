34273ae1dc220897177af51d269bff6b0ac23a45
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-31 12:55:24 +0100

    feat: configure a column and create a reusable custom styled buttol widget

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/answer_button.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -12,6 +14,32 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 20.0),
+          AnswerButton(
+            answerText: 'Anwer 1',
+            onTap: () {},
+          ),
+          const SizedBox(height: 10.0),
+          AnswerButton(
+            answerText: 'Anwer 2',
+            onTap: () {},
+          ),
+          const SizedBox(height: 10.0),
+          AnswerButton(
+            answerText: 'Anwer 3',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
