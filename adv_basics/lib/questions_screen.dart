5fd16b368aed06c772e785ff72e662f0d45ec3f7
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-31 14:13:01 +0100

    feat: acces a question and its answers using list elements and objects to

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -14,28 +15,37 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currenQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currenQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
           const SizedBox(height: 20.0),
           AnswerButton(
-            answerText: 'Anwer 1',
+            answerText: currenQuestion.answers[0],
             onTap: () {},
           ),
           const SizedBox(height: 10.0),
           AnswerButton(
-            answerText: 'Anwer 2',
+            answerText: currenQuestion.answers[1],
             onTap: () {},
           ),
           const SizedBox(height: 10.0),
           AnswerButton(
-            answerText: 'Anwer 3',
+            answerText: currenQuestion.answers[2],
+            onTap: () {},
+          ),
+          const SizedBox(height: 10.0),
+          AnswerButton(
+            answerText: currenQuestion.answers[3],
             onTap: () {},
           ),
         ],
