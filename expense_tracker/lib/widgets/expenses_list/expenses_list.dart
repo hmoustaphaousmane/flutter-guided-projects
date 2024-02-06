0682de0d3166eb850d43305a37d73e27beeff45a
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-02-06 23:16:00 +0100

    feat: create a custom list item with the card

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpenseList extends StatelessWidget {
   const ExpenseList({super.key, required this.expenses});
@@ -12,7 +13,7 @@ class ExpenseList extends StatelessWidget {
     return ListView.builder( // Use `ListView` widget instead of `Column` to improve performance
     // `ListView` is ideal for lists with unkown sizes
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpenseItem(expenses[index]),
     );
   }
 }
