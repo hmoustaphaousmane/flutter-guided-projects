b1ae11011b08e562d748daa5867eca7a26016552
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-07 15:03:43 +0100

    feat: add amount input field to the new expense form

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,18 +13,20 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpense extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   // Always tell flutter to delete the controller if it si not needed any more (this to free the memory)
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const  EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
@@ -32,9 +36,27 @@ class _NewExpense extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
-          Row(children: [ElevatedButton(onPressed: () {
-            debugPrint(_titleController.text);
-          }, child: const Text('Save Expense')),],),
+          TextField(
+            controller: _amountController,
+            decoration: const InputDecoration(prefixText: '\$', label: Text('Amount'),),
+            keyboardType: TextInputType.number,
+            // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  debugPrint(_titleController.text);
+                  debugPrint(_amountController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
+          ),
         ],
       ),
     );
