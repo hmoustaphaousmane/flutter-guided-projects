7f8ae482a046f8e0f0668b0d18e79e4d2f685b85
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-08 13:08:35 +0100

    style: add dark mode theme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,14 +2,40 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
-// Color scheme
+// Color scheme: Light mode
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+// Color scheme: Dark mode
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
+      // Dark mode theme
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            // For some widgets, `styleFrom()` is defined in place of `copyWith()`
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+
+      // Light mode theme
       theme: ThemeData().copyWith(
         // Alway use `copyWith` to override the default theme provided by flutter. This way, we won't need to do all the theming from the ground
         colorScheme: kColorScheme,
@@ -38,6 +64,7 @@ void main() {
               ),
             ),
       ),
+      // themeMode: ThemeMode.system, // Apply the user's system mode (This is set by default by flutter)
       home: const Expenses(),
     ),
   );
