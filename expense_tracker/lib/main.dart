5389d0b4e7bd0f0aa710fb653988450099fa3a02
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-08 15:43:58 +0100

    refactor: lock app orientation to only use portrait

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -14,58 +15,63 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      // Dark mode theme
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 8.0,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            // For some widgets, `styleFrom()` is defined in place of `copyWith()`
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-
-      // Light mode theme
-      theme: ThemeData().copyWith(
-        // Alway use `copyWith` to override the default theme provided by flutter. This way, we won't need to do all the theming from the ground
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 8.0,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            // For some widgets, `styleFrom()` is defined in place of `copyWith()`
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 14.0,
-              ),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        // Dark mode theme
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
             ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              // For some widgets, `styleFrom()` is defined in place of `copyWith()`
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
+
+        // Light mode theme
+        theme: ThemeData().copyWith(
+          // Alway use `copyWith` to override the default theme provided by flutter. This way, we won't need to do all the theming from the ground
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              // For some widgets, `styleFrom()` is defined in place of `copyWith()`
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14.0,
+                ),
+              ),
+        ),
+        // themeMode: ThemeMode.system, // Apply the user's system mode (This is set by default by flutter)
+        home: const Expenses(),
       ),
-      // themeMode: ThemeMode.system, // Apply the user's system mode (This is set by default by flutter)
-      home: const Expenses(),
-    ),
-  );
+    );
+  });
 }
