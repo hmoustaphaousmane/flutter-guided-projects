ecd0f6d138502c0053f3d7f1a46682b1e016038b
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-08 11:03:37 +0100

    feat: save a new expense

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -56,6 +58,14 @@ class _NewExpense extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory.name,
+      ),
+    );
   }
 
   // Always tell flutter to delete the controller if it si not needed any more (this to free the memory)
