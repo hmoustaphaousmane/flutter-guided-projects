452ef2b3b693908a83b3868dc79aec4e1957b8e7
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-08 12:21:43 +0100

    style: set and use a color scheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,10 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+// Color scheme
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        // Alway use `copyWith` to override the default theme provided by flutter. This way, we won't need to do all the theming from the ground
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
