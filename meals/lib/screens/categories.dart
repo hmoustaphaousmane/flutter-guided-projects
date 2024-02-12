6c6c91b753a17f9f57ddf7567af8549cac6d2f66
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-12 16:45:43 +0100

    feat: pass data to the target screen to display the meals of a category

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     // Load a defferent screen
     // Navigator.of(context).push(route) // is equivalent to the next line
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          title: 'Some tiele',
-          meals: [],
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     ); // push route on top of the current stack of screens
@@ -38,9 +43,11 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
           for (final category in availableCategories)
-            CategoryGridItem(category: category, onSelectCategory: () {
-              _selectCategory(context);
-            })
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                })
         ],
       ),
     );
