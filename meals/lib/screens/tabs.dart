3d74dffadfbce06dd8b758df7dfe6a7727b10c3e
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-13 17:07:29 +0100

    feat: setup favarite feature

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -18,13 +18,28 @@ class _TabsScreenState extends State<TabsScreen> {
 
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favorite.');
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('Meal marked as a favorite.');
     }
   }
 
@@ -43,7 +58,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToaggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = 'Your Favorites';
