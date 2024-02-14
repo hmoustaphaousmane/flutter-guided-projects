b9acd89fd67cfcfbcbde4325bd54c5f5f76e3aea
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-14 11:58:36 +0100

    chore: create filters screen and set navigation to it from the side drawer

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
 
@@ -52,9 +53,14 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _setScreen(String identifier) {
     if (identifier == 'filters') {
-      // Close the drawer and go to the filters screen
-
-    } else { // hense identifier == 'mails'
+      // Go to the filters screen
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
+    } else {
+      // hense identifier == 'mails'
       // Close the drawer if the current screen is the meals screen
       Navigator.of(context).pop();
     }
