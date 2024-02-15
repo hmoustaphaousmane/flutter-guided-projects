3f067ec25b38b0894904453546960034531c0391
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-15 12:04:48 +0100

    feat: add an implicit animation to the favortie button's icom

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -37,7 +37,22 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                // Play the animation
+                return RotationTransition(
+                  // turns: animation,
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(
+                    isFavorite), // This key allows to trigger the animation
+              ),
+            ),
           )
         ],
       ),
