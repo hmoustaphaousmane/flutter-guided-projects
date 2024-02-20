a0eb6ad9d7f4b1bccf04943447cdd9a1ffa0cdbe
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-20 12:57:42 +0100

    feat: adding a splash (loading) screen

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,8 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
-
 import 'package:flutter/material.dart';
+import 'package:chat_app/screens/splach.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,6 +29,9 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
           if (snapshot.hasData) {
             return const ChatScreen();
           }
