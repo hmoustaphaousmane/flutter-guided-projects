e38bd17c4efab7715a6b5e584872a72e1126e284
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-21 16:42:29 +0100

    feat: seting up push notifications, requestiong permissions and getting device adress token

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,10 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    // Ask the user for permission to receive and handle push notifications
+    await fcm.requestPermission();
+
+    final token = await fcm.getToken(); // Get the adrees of the device
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +47,9 @@ class ChatScreen extends StatelessWidget {
       ),
       body: const Column(
         children: [
-          Expanded(child: ChatMessages(),),
+          Expanded(
+            child: ChatMessages(),
+          ),
           NewMessage(),
         ],
       ),
